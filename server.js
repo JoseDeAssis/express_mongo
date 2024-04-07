349f4fe834c2e4f90afb6f6ef612c4ed24f09bd1
JoseDeAssis <jose.maria.assis23@gmail.com>
2024-04-06 23:40:31 -0300

    aulas 1 e 2 do curso

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-import http from "http";
+// import http from "http";
+import app from "./src/app.js"
 
 const PORT = 3000;
+const routes = {
+    "/": "Curso de Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Curso Node.js");
-}) 
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end(routes[req.url]);
+// }) 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("servidor escutando");
 })
